8997e049bc213b227b5a091b24da2f15df389bb8
Kisora Thomas <kisora.thomas@gmail.com>
2026-07-22 21:11:50 -0600

    chore: sync from template 267efca

--- src/views/RoomsView.vue
+++ src/views/RoomsView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { siteConfig } from '../config/site.config'
-import HeroSection from '../components/sections/HeroSection.vue'
+import HeroSection from '@apotome/archetype-shared/components/sections/HeroSection.vue'
 import RoomsSection from '../components/sections/RoomsSection.vue'
 import AmenitiesSection from '../components/sections/AmenitiesSection.vue'
 </script>
